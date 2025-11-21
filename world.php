6364cb74039a5fb1cb95cbcdbb0915a1fff94565
Aidan Francis <aidan.francis@my.fiveislands.uwi.edu>
2025-11-21 17:32:49 -0400

    finished Exercises 1,2,3 and 4

--- world.php
+++ world.php
@@ -1,17 +1,40 @@
 <?php
 $host = 'localhost';
-$username = 'lab5_user';
-$password = '';
+$username = 'lab5_user';     
+$password = 'password123';    
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+$country = $_GET['country'] ?? '';  
+
+if (!empty($country)) {
+        $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
+        $stmt->execute(['country' => "%$country%"]);
+    } else {
+        $stmt = $conn->query("SELECT * FROM countries");
+    }
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+
+<table>
+  <thead>
+    <tr>
+      <th>Country Name</th>
+      <th>Continent</th>
+      <th>Indepedence</th>
+      <th>Head of State</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach ($results as $row): ?>
+      <tr>
+        <td><?= $row['name'];?></td>
+        <td><?= $row['continent'];?></td>
+        <td><?= $row['independence_year'];?></td>
+        <td><?= $row['head_of_state'];?></td>
+      </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
